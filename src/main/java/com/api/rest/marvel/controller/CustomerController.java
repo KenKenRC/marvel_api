538ba8a4d41172cf204f226aae50eb020e6c3bcd
Kenia Rodriguez <rodriguezc.kenia@gmail.com>
2023-11-02 20:47:47 -0600

    Avance get por id

--- src/main/java/com/api/rest/marvel/controller/CustomerController.java
+++ src/main/java/com/api/rest/marvel/controller/CustomerController.java
@@ -1,27 +1,35 @@
 package com.api.rest.marvel.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.marvel.client.conect.Connection;
+import com.marvel.client.models.responses.CharactersResponse;
+import com.marvel.client.service.MarvelConnection;
+
+
 
 @RestController
 public class CustomerController {
 
 	//private static Map<Integer, Customer> customers = new HashMap<>(); 
-	
-	   @GetMapping("/customer")  
-	    public String getCustomers() { 
-		   
-		  Connection connection = new Connection();
-		  connection.getComicCharacters();
-		   
-	        return  connection.getComicCharacters(); 
-	    }  
-	   
+
+	@GetMapping("/characters")  
+	public CharactersResponse getCustomers() { 
+
+		MarvelConnection connection = new MarvelConnection();
+		CharactersResponse responses = connection.getComicCharacters();
+		return  responses;
+	}  
+
+	@GetMapping("/characterId")  
+	public CharactersResponse getCustomersById(@RequestParam int id ) { 
+
+		MarvelConnection connection = new MarvelConnection();
+
+		CharactersResponse characterResponse = connection.getComicCharactersId(id);
+
+		return  characterResponse;
+	}  
+
 }
